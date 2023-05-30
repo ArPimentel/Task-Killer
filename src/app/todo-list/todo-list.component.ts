7fa44a6e903956644c1c442223f32a69f439d820
td6904 <td665pro@pm.me>
2023-05-30 12:24:52 +0200

    Logic for editing coding

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class TodoListComponent {
   tasks: string[] = [];
   newTask: string = '';
+  editedTask: string = '';
 
   addTask(){
     if(this.newTask.trim() !== ''){
@@ -16,8 +17,15 @@ export class TodoListComponent {
     }
   }
 
-  editTask() {
+  editTask(task: string) {
     const current = this.tasks.indexOf(task)
+    if(current !== -1 ){
+      if(this.editedTask && this.editedTask.trim() !== ''){
+        this.tasks[current] = this.editedTask.trim()
+      }
+    } else {
+      console.log("Task not found")
+    }
   }
 
   deleteTask(task: string) {
