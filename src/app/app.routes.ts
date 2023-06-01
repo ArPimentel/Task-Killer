659e6a1c1e5573f7f1addd0dcf0deaa46a8ebb86
ArPimentel <pimentel.arturo@gmail.com>
2023-06-01 14:10:27 +0200

    created form list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { TodoListComponent } from "./todo-list/todo-list.component";
+import { CategoryComponent } from "./category/category.component";
 
 const ROUTES: Routes = [
     {path: 'about', component: AboutComponent},
-    {path: '', component: TodoListComponent}
+    {path: '', component: TodoListComponent},
+    {path: 'category', component: CategoryComponent}
 ];
 
 export { ROUTES }
